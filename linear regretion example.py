050cf0897fd44e3f811da2571f52409e429c0203
raeesnoon <142474808+raeesnoon@users.noreply.github.com>
2023-08-18 08:58:01 +0500

    Create linear regretion example.py

---
+++ linear regretion example.py
@@ -0,0 +1,5 @@
+import scipy.stats as stdistance = [1 , 2 , 3 , 4 , 5]temperature = [60 , 58 , 56, 54 ,52]slope, intercept, r, p, se = st.linregress(distance,temperature)
+print("Slope is ",slope)
+print("Intercept is" , intercept)
+print("Equation of temperature profile")equation="T(x) = "+ str(slope) + "X + " + str(intercept)
+print(equation)
